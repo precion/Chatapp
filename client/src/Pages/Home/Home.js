6fb7da3bccd9f6fbf8206d7d39a3524b44d18179
Precion <bistapradip799@gmail.com>
2023-01-15 21:15:03 +0545

    folder structures changed

---
+++ client/src/Pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../../Components/Navbar'
+
+const Home = () => {
+  return (
+    <div>
+        <Navbar/>
+        This is home page.
+    </div>
+  )
+}
+
+export default Home
---
+++ client/src/Pages/Register/Register.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+    <div>
+      Sign up Page
+    </div>
+  )
+}
+
+export default Register
